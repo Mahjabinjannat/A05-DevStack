df261f9c93b36bf93e50324ea14f12f6cb0e82a8
Mahjabin Jannat <mahjabinjannat153@gmail.com>
2026-09-12 18:04:19 +0600

    add-navbar-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { ToastContainer } from "react-toastify";
 import "./App.css";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <ToastContainer />
     </>
   );
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import logoText from '../assets/logo-text.png'
 
 export default function Navbar() {
   return (
-    <div>
-      
+    <div className='flex justify-between p-7 container mx-auto'>
+      <div>
+        <img src={logoText} alt="" />
+      </div>
+      <ul className='flex justify-between gap-5'>
+        <li>Home</li>
+        <li className='text-[#475569]'>Technologies</li>
+        <li className='text-[#475569]'>Projects</li>
+        <li className='text-[#475569]'>About</li>
+        <li className='text-[#475569]'>Contact</li>
+      </ul>
+      <div className='flex gap-5'>
+        <button>Sign In</button>
+        <button>Sign Up</button>
+      </div>
     </div>
   )
 }
