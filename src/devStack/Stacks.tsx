5b57fd854ce9ef0f304a3ba95d9f2a94666085e0
Mahjabin Jannat <mahjabinjannat153@gmail.com>
2026-09-13 20:44:15 +0600

    refactored-Stacks-component

--- src/devStack/Stacks.tsx
+++ src/devStack/Stacks.tsx
@@ -9,25 +9,34 @@ interface IstacksType {
 }
 
 export default function Stacks({ stacks, setStacks }: IstacksType) {
-  if (stacks.length === 0) {
-    return (
-      <div className="space-y-2.5">
-        <h1 className="text-[16px] font-bold text-[#0F172A]">Your Stack</h1>
-        <p className="text-[12px] text-[#94A3B8]">
-          No technologies selected yet.
-        </p>
-        <p className="text-[#94A3B8] text-[12px] border border-dashed border-gray-200 py-6 w-full text-center rounded-[12px] mt-5">
-          Your stack is empty.
-        </p>
-      </div>
-    );
-  }
+  //   if (stacks.length === 0) {
   return (
-    <div className="flex flex-col gap-2">
-      {stacks.map((stack) => (
-        <Stack stack={stack} stacks={stacks} setStacks={setStacks} />
-      ))}
-      <RemoveStack setStacks={setStacks} />
+    <div className="space-y-2.5">
+      <h1 className="text-[16px] font-bold text-[#0F172A] leading-tight">
+        Your Stack
+      </h1>
+      {stacks.length > 0 ? (
+        <div>
+          <p className="text-[12px] text-[#94A3B8] pb-4">
+            {`${stacks.length} technology selected`}
+          </p>
+          <div className="flex flex-col gap-2">
+            {stacks.map((stack) => (
+              <Stack stack={stack} stacks={stacks} setStacks={setStacks} />
+            ))}
+            <RemoveStack setStacks={setStacks} />
+          </div>
+        </div>
+      ) : (
+        <div>
+          <p className="text-[12px] text-[#94A3B8]">
+            No technologies selected yet.
+          </p>
+          <p className="text-[#94A3B8] text-[12px] border border-dashed border-gray-200 py-6 w-full text-center rounded-[12px] mt-5">
+            Your stack is empty.
+          </p>
+        </div>
+      )}
     </div>
   );
 }
